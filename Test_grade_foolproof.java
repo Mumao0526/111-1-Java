84f4d4185aac62799abb734aa700088d1e9fb788
Mumao0526 <75149750+Mumao0526@users.noreply.github.com>
2022-10-07 10:03:36 +0800

    Add files via upload

---
+++ Test_grade_foolproof.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+import static java.lang.System.exit;
+
+public class Test_grade_foolproof {
+    public static void main(String[] args){
+        printStr("Grade!");
+
+        //Create input object,by use Scanner class
+        Scanner InputSomething = new Scanner(System.in);
+
+        System.out.printf("num1 =");
+        String input = InputSomething.nextLine();
+
+        if (ThisStringisInt(input)) {
+            int num1 = Integer.parseInt(input);
+            while (num1!=404) {
+                if(num1 < 101) {
+                    switch (num1 / 10) {
+                        case 10:
+                            printStr("A");
+                            break;
+                        case 9:
+                            printStr("A");
+                            break;
+                        case 8:
+                            printStr("B");
+                            break;
+                        case 7:
+                            printStr("C");
+                            break;
+                        case 6:
+                            printStr("D");
+                            break;
+                        default:
+                            printStr("E");
+                            break;
+                    }
+                    System.out.printf("num1 =");
+                    input = InputSomething.nextLine();
+                    num1 = Integer.parseInt(input);
+                }else {
+                    printStr("Out of 100,bye bye");
+                    exit(1);
+                }
+            }
+        } else {
+            System.out.println("Get out!Key in numbers,ok?");
+        }
+    }
+
+    // Copy from https://stackabuse.com/java-check-if-string-is-a-number/
+    public static boolean ThisStringisInt(String str){
+        try {
+            int intValue = Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    public static void printStr(String str){
+        System.out.println(str);
+    }
+}
