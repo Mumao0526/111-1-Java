0a538a6d372f14c3eaf113374cdbb3a83c154c82
Mumao0526 <75149750+Mumao0526@users.noreply.github.com>
2022-10-07 09:10:49 +0800

    Update Test_grade.java

--- Test_grade.java
+++ Test_grade.java
@@ -2,39 +2,34 @@ import java.util.Scanner;
 
 public class Test_grade {
     public static void main(String[] args){
-        printStr("This's an adder!");
+        printStr("Grade!");
 
         //Create input object,by use Scanner class
         Scanner InputSomething = new Scanner(System.in);
 
-        /* This code is not good */
-        /*
-        System.out.printf("num1 =");
-        int num1 = InputSomething.nextInt();
-        System.out.printf("num2 =");
-        int num2 = InputSomething.nextInt();
-        int sum = num1 + num2;
-        System.out.printf("The sum = %d",sum);
-        */
-
         System.out.printf("num1 =");
         String input = InputSomething.nextLine();
-        if(ThisStringisInt(input)){
-            int num1 = Integer.parseInt(input);
-            if(num1>=90 && num1>=100){
-                printStr("A");
-            }else if(num1>=80 && num1>=89){
-                printStr("B");
-            }else if(num1>=70 && num1>=79){
-                printStr("C");
-            }else if(num1>=60 && num1>=69){
-                printStr("D");
-            }else{
-                printStr("E");
-            }
+        int num1 = Integer.parseInt(input);
 
-        }else {
-            System.out.println("Fuck!Key in numbers,ok?");
+        while (num1<101) {
+            if (ThisStringisInt(input)) {
+                if(num1 >= 90 && num1 <= 100){
+                    printStr("A");
+                }else if(num1 >= 80 && num1 <= 89){
+                    printStr("B");
+                }else if(num1 >= 70 && num1 <= 79){
+                    printStr("C");
+                }else if(num1 >= 60 && num1 <= 69){
+                    printStr("D");
+                }else if(num1 < 60){
+                    printStr("E");
+                }
+            } else {
+                System.out.println("Fuck!Key in numbers,ok?");
+            }
+            System.out.printf("num1 =");
+            input = InputSomething.nextLine();
+            num1 = Integer.parseInt(input);
         }
 
     }
